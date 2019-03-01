8f7a657f005d549091dc00af1efd001e5541903f
Eduardo Nesic <edunesiccp2@icloud.com>
2019-03-01 20:58:45 -0300

    storage migration

--- services/course.go
+++ services/course.go
@@ -1,45 +1,49 @@
 package services
 
 import (
-	"github.com/ednesic/coursemanagement/storage"
+	"github.com/ednesic/coursemanagement/storage/mongodb"
 	"github.com/ednesic/coursemanagement/types"
 )
+
+const coll = "course"
 
 type CourseService interface {
 	Create(types.Course) error
 	Update(types.Course) error
-	FindAll() ([]types.Course,error)
+	FindAll() ([]types.Course, error)
 	Delete(types.Course) error
 	FindOne(string) (types.Course, error)
 }
 
 type CourseServiceImpl struct {
-	storage storage.CourseStorage
+	dal mongodb.DataAccessLayer
 }
 
 func NewCourseService(dbUri, dbName string) (CourseService, error) {
-	st, err := storage.NewCourseStorage(dbUri, dbName)
+	dal, err := mongodb.NewMongoDAL(dbUri, dbName)
 	return &CourseServiceImpl{
-		st,
+		dal,
 	}, err
 }
 
-func (s *CourseServiceImpl) FindOne(name string) (types.Course, error){
-	return s.storage.FindOne(name)
+func (s *CourseServiceImpl) FindOne(name string) (c types.Course, err error) {
+	err = s.dal.FindOne(coll, map[string]interface{}{"name": name}, &c)
+	return
 }
 
 func (s *CourseServiceImpl) Create(course types.Course) error {
-	return s.storage.Create(course)
+	return s.dal.Insert(coll, course)
 }
 
 func (s *CourseServiceImpl) Update(course types.Course) error {
-	return s.storage.Update(course)
+	return s.dal.Upsert(coll, map[string]interface{}{"name": course.Name}, &course)
 }
 
-func (s *CourseServiceImpl) FindAll() ([]types.Course, error) {
-	return s.storage.FindAll()
+func (s *CourseServiceImpl) FindAll() (cs []types.Course, err error) {
+	err = s.dal.Find(coll, map[string]interface{}{}, &cs)
+	return
 }
 
 func (s *CourseServiceImpl) Delete(course types.Course) error {
-	return s.storage.Delete(course)
+	return s.dal.Remove(coll, map[string]interface{}{"name": course.Name})
 }
